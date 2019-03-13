0f4875fdf1f8c688dac743d7b735f1ea730853ec
Mario Zepeda <tecmovilapp@gmail.com>
2019-03-12 18:23:33 -0600

    Adding api vehicles view

---
+++ api/views/vehicles.py
@@ -0,0 +1,24 @@
+
+from django.contrib.auth.models import User
+
+from rest_framework import viewsets, permissions
+from rest_framework_simplejwt import authentication
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import status
+
+from api.serializers.driver import VehicleSerializer
+
+from taxiadmin.models import Vehicle
+
+class VehiclesViewSet(viewsets.ViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = [authentication.JWTAuthentication]
+    def list(self, request):
+        queryset = Vehicle.objects.all()
+        serializer = VehicleSerializer(queryset, many=True)
+        return Response(serializer.data)
+
