5f871aa7c6146ea34fd0ec5618c693ad02fa8fb1
Mario Zepeda <tecmovilapp@gmail.com>
2019-06-10 19:03:27 -0600

    Adding color, model and made to vehicle serializar

--- api/serializers/vehicle.py
+++ api/serializers/vehicle.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 
 
 class VehicleSerializer(serializers.HyperlinkedModelSerializer):
+    model = serializers.CharField(read_only=True)
+    made = serializers.CharField(read_only=True)
     class Meta:
         model = Vehicle
-        fields = ('id', 'register', 'number', 'year',)
+        fields = ('id', 'register', 'number', 'year', 'color', 'model', 'made')
