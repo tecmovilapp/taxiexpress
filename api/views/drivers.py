e973ff0186ef8cedd799215b24e9d99cd4a7aee8
Tecmovil <tecmovilapp@gmail.com>
2019-10-16 04:17:40 +0000

    Fixing CORS issue

--- api/views/drivers.py
+++ api/views/drivers.py
@@ -27,8 +27,8 @@ class DriversViewSet(viewsets.ViewSet):
             obj_driver = Driver.objects.get(user__pk=pk)
             serializer = DriverSerializer(obj_driver, context={'request': request})
             
-            
-            return Response(serializer.data, status='200', headers={'access-control-allow-origin ':'*'})
+            return Response(serializer.data) 
+            #return Response(serializer.data, status='200', headers={'access-control-allow-origin ':'*'})
         except Driver.DoesNotExist:
             return Response(None, status=status.HTTP_404_NOT_FOUND)
 
