5e305e09d13648d9bef98fd849c611c45ff79909
tecmovilapp <tecmovilapp@gmail.com>
2020-03-17 19:08:53 -0600

    Se agrega el rating a endpoint del driver, para que pueda ser usado en las apps.

--- api/serializers/driver.py
+++ api/serializers/driver.py
@@ -7,6 +7,7 @@ from api.serializers.vehicle import VehicleSerializer
 
 class DriverSerializer(serializers.Serializer):
     id = serializers.IntegerField(label='ID', read_only=True)
+    rating = serializers.FloatField(label='RATING', read_only=True)
     identifier = serializers.CharField(max_length=30,  default='')
     picture =  serializers.CharField(max_length=500, default='')
     phone = serializers.CharField(max_length=500, default='')	
