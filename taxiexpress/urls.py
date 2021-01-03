1e49c3e4af89c7e9fa57c87d043b94988879fba2
tecmovilapp <tecmovilapp@gmail.com>
2021-01-03 08:31:07 -0600

    Agregando MerchantURL como parte de los requerimientos de First Atlantic Commerce (FAC) para utilizar sus servicios de pasarela de pagos.

--- taxiexpress/urls.py
+++ taxiexpress/urls.py
@@ -62,4 +62,10 @@ urlpatterns = [
         auth_views.PasswordResetCompleteView.as_view(template_name='admin/registration/password_reset_complete.html'),
         name='password_reset_complete',
     ),
+    
+    url(
+        r'^purchase/done/$',
+        auth_views.PasswordResetCompleteView.as_view(template_name='admin/registration/purchase_done.html'),
+        name='purchase_done',
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
